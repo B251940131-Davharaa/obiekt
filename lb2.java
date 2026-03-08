8e6096f029fa75a8c1e3f4e612ea9388e4e50372
B251940131-Davharaa <b251940131@erdenetis.edu.mn>
2026-03-08 13:39:54 +0800

    Add files via upload

---
+++ lb2.java
@@ -0,0 +1,27 @@
+package dgii;
+import java.util.Scanner;
+public class lb2 {
+	
+	          public static void main(String[] args) {
+			  Scanner input = new Scanner(System.in);
+			  
+			    double  L, bvslvvr, r, P=Math.PI;
+			    
+			    System.out.print("radiusaa oruul: "); 
+			    r = input.nextInt();
+
+			   
+			    L=2*P*r;
+			    bvslvvr=3*L;
+
+			    System.out.printf("toirgiin urt %f\n", L); 
+			    System.out.printf("busluurin urt %f", bvslvvr); 
+			    
+			    
+
+			    input.close();  
+		  }
+		}
+		
+
+
